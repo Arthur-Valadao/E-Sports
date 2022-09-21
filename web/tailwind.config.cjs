00a8d8993f9f8e4ebc3e162439388fb75a645b68
Arthur <Arth.valadao@gmail.com>
2022-09-21 00:43:15 -0400

    insert landing page

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -7,7 +7,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage:{
-        galaxy: "url('/background-galaxy.png')"
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 10.0%, #43E7AD 80.0%, #E1D55D 10.0%)',
+        'game-gradient':'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
   },
