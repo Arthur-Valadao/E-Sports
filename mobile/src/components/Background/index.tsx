d97cca5d640b21943d4e2c49559f345f347c46a0
Arthur <Arth.valadao@gmail.com>
2022-09-21 23:05:18 -0400

    install librarys

--- mobile/src/components/Background/index.tsx
+++ mobile/src/components/Background/index.tsx
@@ -1,7 +1,7 @@
 import { ImageBackground } from 'react-native';
 import { styles } from './styles';
 
-import backgroundImg from '../../assets/background=galaxy.png';
+import backgroundImg from '../../assets/background-galaxy.png';
 
 interface Props {
   children: React.ReactNode;
@@ -11,8 +11,8 @@ export function Background({ children }: Props) {
   return (
     <ImageBackground
       source={backgroundImg}
-      style={styles.container}
       defaultSource={backgroundImg}
+      style={styles.container}
     >
       {children}
     </ImageBackground>
